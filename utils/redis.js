d9ba5a248128824b6ffbb7a5b8a19611a167d5e1
Jamie897 <jamiedanielle456@gmail.com>
2024-03-14 10:36:12 -0500

    utils

---
+++ utils/redis.js
@@ -0,0 +1,57 @@
+// utils/redis.js
+
+const redis = require('redis');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+
+        this.client.on('error', (err) => {
+            console.error('Redis error:', err);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+    async set(key, value, durationInSeconds) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, durationInSeconds, value, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply);
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
