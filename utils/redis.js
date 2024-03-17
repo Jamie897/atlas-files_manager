1d75a11f9df2f79b3110b72613864a838e9222aa
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-03-17 12:37:19 -0500

    task 3

--- utils/redis.js
+++ utils/redis.js
@@ -1,14 +1,16 @@
-// utils/redis.js
-
 const redis = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
-
-    this.client.on('error', (err) => {
-      console.error('Redis error:', err);
+    this.client = redis.createClient({
     });
+
+    this.client.on('error', (err) => console.error(err));
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
@@ -16,42 +18,17 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, reply) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(reply);
-      });
-    });
+    return this.getAsync(key);
   }
 
-  async set(key, value, durationInSeconds) {
-    return new Promise((resolve, reject) => {
-      this.client.setex(key, durationInSeconds, value, (err, reply) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(reply);
-      });
-    });
+  async set(key, value, duration) {
+    return this.setAsync(key, value, 'EX', duration);
   }
 
   async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (err, reply) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(reply);
-      });
-    });
+    return this.delAsync(key);
   }
 }
 
 const redisClient = new RedisClient();
-
 module.exports = redisClient;
